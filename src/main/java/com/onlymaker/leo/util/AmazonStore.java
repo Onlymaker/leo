e76d852b56f898ec0f005474660fb13621425d81
jibo <jibo@outlook.com>
2017-11-01 10:47:04 +0800

    Remove some stores

--- src/main/java/com/onlymaker/leo/util/AmazonStore.java
+++ src/main/java/com/onlymaker/leo/util/AmazonStore.java
@@ -9,15 +9,9 @@ import java.util.Map;
 
 public class AmazonStore {
     public static final Map<String, String> STORES = new HashMap<String, String>() {{
-        put("AOUS", "https://www.amazon.com/dp/");
-        put("AOCA", "https://www.amazon.ca/dp/");
-        put("AOUK", "https://www.amazon.co.uk/dp/");
         put("AODE", "https://www.amazon.de/dp/");
-        put("AOFR", "https://www.amazon.fr/dp/");
-        put("AOES", "https://www.amazon.es/dp/");
-        put("AFUS", "https://www.amazon.com/dp/");
         put("AKEU", "https://www.amazon.de/dp/");
-        put("ACUS", "https://www.amazon.com/dp/");
         put("AHUS", "https://www.amazon.com/dp/");
+        put("ACUS", "https://www.amazon.com/dp/");
     }};
 }
